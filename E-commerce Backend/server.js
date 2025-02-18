ddc8d9b260aeb0c78fec73877a3c23f4277deb99
Chamika Jayarathna <failgames.lk@gmail.com>
2025-02-18 11:04:01 +0530

    setup database connection

--- E-commerce Backend/server.js
+++ E-commerce Backend/server.js
@@ -1,7 +1,19 @@
 import express from 'express';
+import mongoose from "mongoose";
+import "dotenv/config";
 
 const app = express();
 const PORT = 3000;
+
+mongoose.connect(process.env.DATABASE_URL)
+.then(() => {
+    console.log("Connected to MongoDB");
+})
+.catch(err => {
+    console.log(err);
+})
+
+app.use(express.json());
 
 
 app.listen(PORT, () => {
