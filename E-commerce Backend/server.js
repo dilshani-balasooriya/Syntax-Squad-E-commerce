56de4d0650243c007f787774b054dfb6ffd2a8d0
Chamika Jayarathna <failgames.lk@gmail.com>
2025-02-25 14:12:40 +0530

    setup cors

--- E-commerce Backend/server.js
+++ E-commerce Backend/server.js
@@ -1,10 +1,26 @@
 import express from 'express';
 import "dotenv/config";
+import cors from 'cors';
 import DBConnection from './config/dbConnection.js';
 
 const app = express();
 const PORT = 3000;
 
+app.use(
+    cors({
+      origin: "http://localhost:5173",
+      methods: ["GET", "POST", "DELETE", "PUT"],
+      allowedHeaders: [
+        "Content-Type",
+        "Authorization",
+        "Cache-Control",
+        "Expires",
+        "Pragma",
+      ],
+      credentials: true,
+    })
+);
+app.use(cookieParser());
 app.use(express.json());
 
 
