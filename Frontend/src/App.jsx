0b224ab26a1dbd54f2a052d948bc50f03c9797e5
Chamika Jayarathna <failgames.lk@gmail.com>
2025-02-28 16:58:14 +0530

    create header.jsx

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -2,8 +2,16 @@ import React from "react";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import Home from "./Home";
 import Contact from "./Contact";
+import { ClerkProvider } from "@clerk/clerk-react";
 
 const App = () => {
+
+  const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY
+
+  if (!PUBLISHABLE_KEY) {
+    throw new Error("Missing Publishable Key")
+  }
+
   const router = createBrowserRouter([
     {
       path: "/",
@@ -15,7 +23,11 @@ const App = () => {
     },
   ]);
 
-  return <RouterProvider router={router} />;
+  return (
+    <ClerkProvider publishableKey={PUBLISHABLE_KEY} afterSignOutUrl="/">
+      <RouterProvider router={router} />
+    </ClerkProvider>
+  )
 };
 
 export default App;
