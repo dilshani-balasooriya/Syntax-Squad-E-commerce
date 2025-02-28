af423cff6e37f602440e107e8937cb128c69fba5
Chamika Jayarathna <failgames.lk@gmail.com>
2025-02-28 18:53:59 +0530

    update search.jsx v1

--- Frontend/src/components/Search.jsx
+++ Frontend/src/components/Search.jsx
@@ -1,8 +1,51 @@
 import React from 'react';
+import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from './ui/select';
+import { Separator } from './ui/separator';
+import { CiSearch } from "react-icons/ci";
 
 const Search = () => {
   return (
-    <div>Search</div>
+    <div className='p-2 md:p-5 bg-white rounded-md md:rounded-full flex-col md:flex md:flex-row gap-10 px-5 items-center w-[60%]'>
+        <Select>
+            <SelectTrigger  className="outline-none md:border-none w-full shadow-none text-lg">
+                <SelectValue placeholder="Cars" />
+            </SelectTrigger>
+            <SelectContent>
+                <SelectItem value="New">New</SelectItem>
+                <SelectItem value="Used">Used</SelectItem>
+                <SelectItem value="Certified Pre-Owned">Certified Pre-Owned</SelectItem>
+            </SelectContent>
+        </Select>
+
+        <Separator orientation="vertical"  className="hidden md:block" />
+
+        <Select>
+            <SelectTrigger  className="outline-none md:border-none w-full shadow-none text-lg">
+                <SelectValue placeholder="Car Makes" />
+            </SelectTrigger>
+            <SelectContent>
+                <SelectItem value="New">New</SelectItem>
+                <SelectItem value="Used">Used</SelectItem>
+                <SelectItem value="Certified Pre-Owned">Certified Pre-Owned</SelectItem>
+            </SelectContent>
+        </Select>
+
+        <Separator orientation="vertical"  className="hidden md:block" />
+
+        <Select>
+            <SelectTrigger  className="outline-none md:border-none w-full shadow-none text-lg">
+                <SelectValue placeholder="Pricing" />
+            </SelectTrigger>
+            <SelectContent>
+                <SelectItem value="New">New</SelectItem>
+                <SelectItem value="Used">Used</SelectItem>
+                <SelectItem value="Certified Pre-Owned">Certified Pre-Owned</SelectItem>
+            </SelectContent>
+        </Select>
+        <div>
+            <CiSearch className='text-[50px] bg-primary rounded-full p-3 text-white hover:scale-105 transition-all cursor-pointer'/>
+        </div>
+    </div>
   );
 }
 
