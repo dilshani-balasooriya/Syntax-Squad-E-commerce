d327553caf1194e13a7c44bc71ca95aa1e908d30
Chamika Jayarathna <failgames.lk@gmail.com>
2025-02-28 19:51:03 +0530

    update MostSearchedCar.jsx v1

--- Frontend/src/components/CarItem.jsx
+++ Frontend/src/components/CarItem.jsx
@@ -1,8 +1,14 @@
 import React from 'react';
+  
 
-const CarItem = () => {
+const CarItem = ({car}) => {
   return (
-    <div>CarItem</div>
+    <div>
+        <img src={car?.image} width={'100%'} height={250} className='rounded-t-xl h-[180px] object-cover'/>
+        <div className="p-4">
+            <h2 className='font-bold text-black text-lg mb-2'>{car?.name} Miles</h2> 
+        </div>
+    </div>
   );
 }
 
--- Frontend/src/components/MostSearchedCar.jsx
+++ Frontend/src/components/MostSearchedCar.jsx
@@ -1,13 +1,23 @@
 import FakeData from '@/Shared/FakeData';
 import React from 'react';
+import CarItem from './CarItem';
+import { Carousel, CarouselContent, CarouselItem, CarouselNext, CarouselPrevious } from "./ui/carousel"
 
 const MostSearchedCar = () => {
   return (
     <div className='mx-24 hidden md:block'>
         <h2 className='font-bold text-3xl text-center mt-16 mb-7'>Most Searched Cars</h2>
-        {/* {FakeData.carList.map((car, index) => (
-
-        ))} */}
+        <Carousel>
+            <CarouselContent>
+                {FakeData.carList.map((car, index) => (
+                    <CarouselItem className="basis-1/2 md:basis-1/3 lg:basis-1/4">
+                        <CarItem car={car} />
+                    </CarouselItem>
+                ))}
+            </CarouselContent>
+            <CarouselPrevious />
+            <CarouselNext />
+        </Carousel>
     </div>
   );
 }
