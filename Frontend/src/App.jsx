641e350d81ed82029e40622a4341b6ca99d48ae9
Chamika Jayarathna <failgames.lk@gmail.com>
2025-02-28 16:50:10 +0530

    create contact.jsx

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,19 +1,21 @@
-import React from 'react';
-import { createBrowserRouter, RouterProvider } from 'react-router-dom';
-import Home from './Home';
+import React from "react";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Home from "./Home";
+import Contact from "./Contact";
 
 const App = () => {
-
-  const router=createBrowserRouter([
+  const router = createBrowserRouter([
     {
-      path:'/',
-      element:<Home/>
+      path: "/",
+      element: <Home />,
+    },
+    {
+      path:'/contact',
+      element:<Contact/>
     },
   ]);
 
-  return (
-    <RouterProvider router={router} />
-  );
-}
+  return <RouterProvider router={router} />;
+};
 
 export default App;
