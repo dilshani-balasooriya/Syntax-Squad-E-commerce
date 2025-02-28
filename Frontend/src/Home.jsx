f70795f2adab94a5baa8763aac2e504ffd897c5c
Chamika Jayarathna <failgames.lk@gmail.com>
2025-02-28 19:29:00 +0530

    create MostSearchedCar.jsx

--- Frontend/src/Home.jsx
+++ Frontend/src/Home.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import Header from './components/Header';
 import Hero from './components/Hero';
 import Category from './components/Category';
+import MostSearchedCar from './components/MostSearchedCar';
 
 const Home = () => {
   return (
@@ -9,6 +10,7 @@ const Home = () => {
       <Header/>
       <Hero/>
       <Category/>
+      <MostSearchedCar/>
     </div>
   );
 }
---
+++ Frontend/src/components/MostSearchedCar.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const MostSearchedCar = () => {
+  return (
+    <div>MostSearchedCar</div>
+  );
+}
+
+export default MostSearchedCar;
