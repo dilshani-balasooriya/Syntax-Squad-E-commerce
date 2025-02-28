36268e969b2e1bd65d55c352d457297eb78d8e72
Chamika Jayarathna <failgames.lk@gmail.com>
2025-02-28 19:33:14 +0530

    create CarItem.jsx

--- Frontend/src/components/MostSearchedCar.jsx
+++ Frontend/src/components/MostSearchedCar.jsx
@@ -1,8 +1,14 @@
+import FakeData from '@/Shared/FakeData';
 import React from 'react';
 
 const MostSearchedCar = () => {
   return (
-    <div>MostSearchedCar</div>
+    <div className='mx-24 hidden md:block'>
+        <h2 className='font-bold text-3xl text-center mt-16 mb-7'>Most Searched Cars</h2>
+        {/* {FakeData.carList.map((car, index) => (
+
+        ))} */}
+    </div>
   );
 }
 
