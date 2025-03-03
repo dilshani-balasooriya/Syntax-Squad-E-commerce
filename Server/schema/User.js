2ded434373cb00b71c959aaad838c4944bb5e073
Chamika Jayarathna <failgames.lk@gmail.com>
2025-03-03 11:38:01 +0530

    Create basic user schema

---
+++ Server/schema/User.js
@@ -0,0 +1,23 @@
+import mongoose from "mongoose";
+
+const userSchema = mongoose.Schema({
+    clerkId: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    email: {
+        type: String, 
+        required: true 
+    },
+    role: { 
+        type: String, 
+        default: "user" 
+    },
+    google_auth: {
+        type: Boolean,
+        default: false
+    },
+});
+
+export default mongoose.model("user", userSchema);
