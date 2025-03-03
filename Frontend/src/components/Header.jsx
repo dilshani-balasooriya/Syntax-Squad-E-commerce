039d4a352ce168b323e16b29dbd821388fc57574
Chamika Jayarathna <failgames.lk@gmail.com>
2025-03-03 17:25:03 +0530

    usdate tyo set data mongodb

--- Frontend/src/components/Header.jsx
+++ Frontend/src/components/Header.jsx
@@ -1,34 +1,67 @@
-import { SignInButton, UserButton, useUser } from '@clerk/clerk-react';
-import React from 'react';
-import { Button } from './ui/button';
+import { SignInButton, useAuth, UserButton, useUser } from "@clerk/clerk-react";
+import React, { useEffect } from "react";
+import { Button } from "./ui/button";
+import apiRequest from "@/lib/apiRequest";
 
 const Header = () => {
+  const { user, isSignedIn } = useUser();
+  const { getToken } = useAuth();
 
-    const {user, isSignedIn}=useUser();
+  const registerUser = async () => {
+    if (isSignedIn) {
+      const token = await getToken();
+      const isGoogleAuth = user.externalAccounts.some(
+        (account) => account.provider === "google"
+      );
+
+      const res = await apiRequest.post(
+        "/auth/login",
+        {
+          clerkId: user.id,
+          email: user.primaryEmailAddress.emailAddress,
+          isGoogleAuth,
+        },
+        {
+          headers: { Authorization: `Bearer ${token}` },
+        }
+      );
+      localStorage.setItem("token", res.data.token);
+    }
+  };
+
+  useEffect(() => {
+    registerUser();
+  }, [isSignedIn, user]);
 
   return (
-    <div className='flex justify-between items-center shadow-sm p-5'>
-        <img src='/logo.svg' width={150} height={100} />
-        <ul className='hidden  md:flex gap-16'>
-            <li className='font-medium hover:scale-105 transition-all cursor-pointer hover:text-primary'>Home</li>
-            <li className='font-medium hover:scale-105 transition-all cursor-pointer hover:text-primary'>Search</li>
-            <li className='font-medium hover:scale-105 transition-all cursor-pointer hover:text-primary'>New</li>
-            <li className='font-medium hover:scale-105 transition-all cursor-pointer hover:text-primary'>Preowned</li>
-        </ul>
+    <div className="flex justify-between items-center shadow-sm p-5">
+      <img src="/logo.svg" width={150} height={100} />
+      <ul className="hidden  md:flex gap-16">
+        <li className="font-medium hover:scale-105 transition-all cursor-pointer hover:text-primary">
+          Home
+        </li>
+        <li className="font-medium hover:scale-105 transition-all cursor-pointer hover:text-primary">
+          Search
+        </li>
+        <li className="font-medium hover:scale-105 transition-all cursor-pointer hover:text-primary">
+          New
+        </li>
+        <li className="font-medium hover:scale-105 transition-all cursor-pointer hover:text-primary">
+          Preowned
+        </li>
+      </ul>
 
-        {isSignedIn? 
-            <div className='flex items-center gap-5'>
-                <UserButton/>
-                <Button>Submit Listing</Button>
-            </div>
-            :
-            <SignInButton mode="modal">
-                <Button>Submit Listing</Button>
-            </SignInButton>
-        }
-
+      {isSignedIn ? (
+        <div className="flex items-center mr-5">
+          <UserButton />
+        </div>
+      ) : (
+        <SignInButton mode="modal">
+          <Button>Join</Button>
+        </SignInButton>
+      )}
     </div>
   );
-}
+};
 
 export default Header;
