89c54878dbaec5946c103539047344e4752319d0
Chamika Jayarathna <failgames.lk@gmail.com>
2025-03-03 17:24:41 +0530

    update auth route and auth controller v2

--- Server/controller/auth-controller.js
+++ Server/controller/auth-controller.js
@@ -3,13 +3,18 @@ import User from "../schema/User.js";
 
 export const Login = async (req, res) => {
   try {
-    const { clerkId, email } = req.body;
+    const { clerkId, email, isGoogleAuth } = req.body;
 
     let user = await User.findOne({ clerkId });
 
     if (!user) {
-      user = new User({ clerkId, email });
+      user = new User({ clerkId, email, google_auth: isGoogleAuth || false });
       await user.save();
+    } else {
+      if (isGoogleAuth && !user.google_auth) {
+        user.google_auth = true;
+        await user.save();
+      }
     }
 
     const token = jwt.sign(
