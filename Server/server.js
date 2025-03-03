156e083f96b8d2747467cbfd460b4ecabef39cf7
Chamika Jayarathna <failgames.lk@gmail.com>
2025-03-03 12:15:27 +0530

    import auth route in server.js

--- Server/server.js
+++ Server/server.js
@@ -2,6 +2,7 @@ import express from 'express';
 import "dotenv/config";
 import cors from 'cors';
 import DBConnection from './config/dbConnection.js';
+import authRoute from './routes/auth-route.js';
 
 const app = express();
 const PORT = 3000;
@@ -9,6 +10,7 @@ const PORT = 3000;
 app.use(cors({}));
 app.use(express.json());
 
+app.use('/api/auth', authRoute);
 
 app.listen(PORT, () => {
     DBConnection();
