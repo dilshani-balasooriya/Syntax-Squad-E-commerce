685c1baca0bd823b8b41d686873604737a54d02c
Chamika Jayarathna <failgames.lk@gmail.com>
2025-03-04 11:13:25 +0530

    create InputField.jsx

--- Frontend/src/admin/add-listing/AddListing.jsx
+++ Frontend/src/admin/add-listing/AddListing.jsx
@@ -5,6 +5,18 @@ const AddListing = () => {
   return (
     <div>
       <AdminHeader title={"Add Listing"}/>
+      <div className="px-10 md:px-20 my-10">
+        <h2 className='font-bold text-4xl'>Add New Listing</h2>
+        <form className='p-10 border rounded-xl mt-10'>
+          {/* Car Details  */}
+          <div>
+            <h2 className='font-medium text-xl mb-6'>Car Details</h2>
+          </div>
+
+          {/* Features List  */}
+          {/* Car Images  */}
+        </form>
+      </div>
     </div>
   )
 }
