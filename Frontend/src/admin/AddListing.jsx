3d2bc9ad59b92fa9a0736443605c09ebc9cd52ec
Chamika Jayarathna <failgames.lk@gmail.com>
2025-03-04 08:34:23 +0530

    create admin part UI

--- Frontend/src/admin/AddListing.jsx
+++ Frontend/src/admin/AddListing.jsx
@@ -2,7 +2,16 @@ import React from 'react';
 
 const AddListing = () => {
   return (
-    <div>AddListing</div>
+    <div>
+        <div className="px-10 md:px-20 my-10">
+            <h2 className='font-bold text-4xl'>Add New Listing</h2>
+            <form className='p-10 border rounded-xl mt-10'>
+                {/* Car Details  */}
+                {/* Features List */}
+                {/* Car Images */}
+            </form>
+        </div>
+    </div>
   );
 }
 
--- Frontend/src/pages/Profile.jsx
+++ Frontend/src/pages/Profile.jsx
@@ -1,6 +1,7 @@
 import Header from '@/components/Header';
 import { Button } from '@/components/ui/button';
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Profile = () => {
   return (
@@ -9,7 +10,9 @@ const Profile = () => {
         <div className="px-10 md:px-20 my-10">
             <div className="flex justify-between items-center">
                 <h2 className='font-bold text-4xl'>My Listing</h2>
-                <Button>+ Add New Listing</Button>
+                <Link to={'/add-listing'}>
+                    <Button>+ Add New Listing</Button>
+                </Link>
             </div>
         </div>
     </div>
