0214f7f4a355f259a6bb8cfea8b3ffa1a07d9755
Chamika Jayarathna <failgames.lk@gmail.com>
2025-03-04 13:14:53 +0530

    update add listing v1

--- Frontend/src/admin/add-listing/AddListing.jsx
+++ Frontend/src/admin/add-listing/AddListing.jsx
@@ -4,6 +4,10 @@ import carDetails from "../../Shared/carDetails.json";
 import InputField from "./components/InputField";
 import DropdownField from "./components/DropdownField";
 import TextAreaField from "./components/TextAreaField";
+import { Separator } from "@/components/ui/separator";
+import features from '../../Shared/features.json';
+import { Checkbox } from "@/components/ui/checkbox";
+import { Button } from "@/components/ui/button";
 
 const AddListing = () => {
   return (
@@ -33,9 +37,24 @@ const AddListing = () => {
               ))}
             </div>
           </div>
+          <Separator className="my-6"/>
 
           {/* Features List  */}
+          <div className="">
+            <h2 className='font-medium text-xl my-6'>Features</h2>
+            <div className='grid grid-cols-2 md:grid-cols-3 gap-2'>
+              {features.features.map((item,index) => (
+                <div key={index} className='flex gap-2 items-center'>
+                  <Checkbox/> <h2>{item.label}</h2>
+                </div>
+              ))}
+            </div>
+          </div>
+          <Separator className="my-6"/>
           {/* Car Images  */}
+          <div className="mt-10 flex justify-end">
+            <Button className="bg-red-500">Submit</Button>
+          </div>
         </form>
       </div>
     </div>
