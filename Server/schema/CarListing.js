5480b7b28396aacd4e627c765684219565cb2a4d
Chamika Jayarathna <failgames.lk@gmail.com>
2025-03-04 17:35:22 +0530

    create carlisting schema

---
+++ Server/schema/CarListing.js
@@ -0,0 +1,88 @@
+import mongoose from 'mongoose';
+
+const carListingSchema = new mongoose.Schema({
+    listingTitle: { 
+        type: String,
+        required: true
+    },
+    tagline: { 
+        type: String 
+    },
+    originalPrice: { 
+        type: String 
+    },
+    sellingPrice: { 
+        type: String, 
+        required: true 
+    },
+    category: { 
+        type: String, 
+        enum: ["Sedan", "SUV", "Truck", "Coupe", "Convertible", "Van", "Hatchback", "Electric", "Hybrid"],
+        required: true 
+    },
+    condition: { 
+        type: String, 
+        enum: ["New", "Used", "Certified Pre-Owned"],
+        required: true 
+    },
+    make: { 
+        type: String, 
+        enum: ["Toyota", "Honda", "Ford", "Chevrolet", "Nissan", "BMW", "Mercedes-Benz", "Audi", "Tesla", "Volkswagen", "Hyundai", "Kia", "Lexus", "Subaru", "Mazda", "Jeep", "Dodge", "Ram", "GMC", "Volvo", "Land Rover", "Jaguar", "Porsche", "Fiat", "Alfa Romeo", "Mitsubishi", "Infiniti", "Acura", "Mini", "Buick", "Cadillac", "Chrysler", "Lincoln", "Maserati", "Bentley", "Rolls-Royce", "Aston Martin", "Ferrari", "Lamborghini", "McLaren", "Pagani", "Bugatti"],
+        required: true 
+    },
+    model: { 
+        type: String, 
+        required: true 
+    },
+    year: { 
+        type: Number, 
+        required: true 
+    },
+    driveType: { 
+        type: String, 
+        enum: ["FWD", "RWD", "AWD", "4WD"],
+        required: true 
+    },
+    transmission: { 
+        type: String, 
+        enum: ["Automatic", "Manual", "CVT"],
+        required: true 
+    },
+    fuelType: { 
+        type: String, 
+        enum: ["Petrol", "Diesel", "Electric", "Hybrid"],
+        required: true 
+    },
+    mileage: { 
+        type: Number, 
+        required: true 
+    },
+    engineSize: { 
+        type: String 
+    },
+    cylinder: { 
+        type: Number 
+    },
+    color: { 
+        type: String, 
+        enum: ["Black", "Blue", "Gold", "Pink", "Red", "Silver", "White"],
+        required: true 
+    },
+    door: { 
+        type: Number, 
+        required: true 
+    },
+    vin: { 
+        type: String 
+    },
+    offerType: { 
+        type: String,
+        enum: ["Buy", "Hot Offer", "Sell", "Urgent"],
+    },
+    listingDescription: { 
+        type: String, 
+        required: true 
+    },
+});
+
+export default mongoose.model("carListing", carListingSchema);
