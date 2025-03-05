c3b7603fdca3c1d44b0b1b47f960624984678976
Chamika Jayarathna <failgames.lk@gmail.com>
2025-03-05 06:58:49 +0530

    update add listing v3(Frontendp)

--- Frontend/src/lib/apiRequest.js
+++ Frontend/src/lib/apiRequest.js
@@ -1,7 +1,15 @@
 import axios from "axios";
 
 const apiRequest = axios.create({
-    baseURL: "http://localhost:3000/api",
+  baseURL: "http://localhost:3000/api",
+});
+
+apiRequest.interceptors.request.use((config) => {
+  const token = localStorage.getItem("token");
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`;
+  }
+  return config;
 });
 
 export default apiRequest;
