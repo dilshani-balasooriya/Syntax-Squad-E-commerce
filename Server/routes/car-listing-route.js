0585f3ec365529216a1237599b5a1fa10b007672
Chamika Jayarathna <failgames.lk@gmail.com>
2025-03-06 20:41:01 +0530

    update get single car listing route

--- Server/routes/car-listing-route.js
+++ Server/routes/car-listing-route.js
@@ -7,7 +7,7 @@ const router = express.Router();
 router.post('/create-listing', verifyToken, CreateCarListing);
 router.get('/get-all-listing', GetAllCarListing);
 router.get('/get-user-listing', verifyToken, GetUserCarListing);
-router.get('/get-single-car-listing/:id', GetSingleCarListing);
+router.get('/get-single-car-listing/:id', verifyToken, GetSingleCarListing);
 router.put('/edit-car-list/:id', verifyToken, EditCarListing);
 
 export default router;
