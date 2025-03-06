11421fe7a83c33ea778703edb7038729044eca7c
Chamika Jayarathna <failgames.lk@gmail.com>
2025-03-06 09:05:48 +0530

    create get all car listing route

--- Server/controller/car-listing-controller.js
+++ Server/controller/car-listing-controller.js
@@ -1,39 +1,74 @@
-import CarListing from '../schema/CarListing.js';
+import CarListing from "../schema/CarListing.js";
 
 export const CreateCarListing = async (req, res) => {
-    const { listingTitle, tagline, originalPrice, sellingPrice, category, condition, make, model, year, driveType, transmission, fuelType, mileage, engineSize, cylinder, color, door, vin, offerType, listingDescription, features, imageUrl } = req.body;
+  const {
+    listingTitle,
+    tagline,
+    originalPrice,
+    sellingPrice,
+    category,
+    condition,
+    make,
+    model,
+    year,
+    driveType,
+    transmission,
+    fuelType,
+    mileage,
+    engineSize,
+    cylinder,
+    color,
+    door,
+    vin,
+    offerType,
+    listingDescription,
+    features,
+    imageUrl,
+  } = req.body;
 
-    try {
-        const newCarListing = new CarListing({
-            listingTitle,
-            tagline,
-            originalPrice,
-            sellingPrice,
-            category,
-            condition,
-            make,
-            model,
-            year,
-            driveType,
-            transmission,
-            fuelType,
-            mileage,
-            engineSize,
-            cylinder,
-            color,
-            door,
-            vin,
-            offerType,
-            listingDescription,
-            features,
-            imageUrl,
-            userId: req.user.id
-        });
+  try {
+    const newCarListing = new CarListing({
+      listingTitle,
+      tagline,
+      originalPrice,
+      sellingPrice,
+      category,
+      condition,
+      make,
+      model,
+      year,
+      driveType,
+      transmission,
+      fuelType,
+      mileage,
+      engineSize,
+      cylinder,
+      color,
+      door,
+      vin,
+      offerType,
+      listingDescription,
+      features,
+      imageUrl,
+      userId: req.user.id,
+    });
 
-        await newCarListing.save();
-        return res.status(201).json("Save listing successfully!");
+    await newCarListing.save();
+    return res.status(201).json("Save listing successfully!");
+  } catch (error) {
+    return res
+      .status(500)
+      .json({ error: "Server error, please try again later." });
+  }
+};
 
-    } catch (error) {
-        return res.status(500).json({ error: 'Server error, please try again later.' });
-    }
-}
+export const GetAllCarListing = async (req, res) => {
+  try {
+    const carListings = await CarListing.find();
+    return res.status(200).json(carListings);
+  } catch (error) {
+    return res
+      .status(500)
+      .json({ error: "Server error, please try again later." });
+  }
+};
--- Server/routes/car-listing-route.js
+++ Server/routes/car-listing-route.js
@@ -1,9 +1,10 @@
 import express from 'express';
-import { CreateCarListing } from '../controller/car-listing-controller.js';
+import { CreateCarListing, GetAllCarListing } from '../controller/car-listing-controller.js';
 import verifyToken from '../middleware/verifyToken.js';
 
 const router = express.Router();
 
 router.post('/create-listing', verifyToken, CreateCarListing);
+router.get('/get-all-listing', GetAllCarListing);
 
 export default router;
