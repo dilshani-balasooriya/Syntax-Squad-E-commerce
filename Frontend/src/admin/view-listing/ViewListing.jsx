d952a33b68dfba99f1bac617fe11ceb18b4b9dfc
Chamika Jayarathna <failgames.lk@gmail.com>
2025-03-06 11:32:45 +0530

    update the first ui on CarItem.jsx v1

--- Frontend/src/admin/view-listing/ViewListing.jsx
+++ Frontend/src/admin/view-listing/ViewListing.jsx
@@ -1,13 +1,33 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import AdminHeader from '../AdminHeader';
 import CarItem from './components/CarItem';
+import apiRequest from '@/lib/apiRequest';
 
 const ViewListing = () => {
+    const [carListings, setCarListings] = useState([]);
+
+    const GetUserCarListing = async () => {
+        try {
+          const response = await apiRequest.get("/car-listing/get-user-listing");
+          setCarListings(response.data);
+        } catch (error) {
+        //   toast.error("Failed to load listings!");
+        }
+      };
+
+      useEffect(() => {
+          GetUserCarListing();
+        }, []);
+
   return (
     <div>
         <AdminHeader title={"View Listing"}/>
         <div className="p-8 grid grid-row-2 md:grid-row-3 lg:grid-row-4 mt-7 gap-5">
-            <CarItem/>
+            {carListings.map((item, index) => (
+            <div key={index}>
+              <CarItem car={item} />
+            </div>
+          ))}
         </div>
     </div>
   );
--- Frontend/src/admin/view-listing/components/CarItem.jsx
+++ Frontend/src/admin/view-listing/components/CarItem.jsx
@@ -1,6 +1,6 @@
+import React from 'react';
 import { Button } from '@/components/ui/button';
 import { Separator } from '@/components/ui/separator';
-import React from 'react';
 import { FaEdit, FaEye, FaTrashAlt } from 'react-icons/fa';
 import { GiGearStickPattern } from 'react-icons/gi';
 import { LuFuel } from 'react-icons/lu';
@@ -9,25 +9,25 @@ import { TbBrandSpeedtest } from 'react-icons/tb';
 const CarItem = ({ car }) => {
   return (
     <div className="flex items-center rounded-xl bg-white border hover:shadow-lg transition-shadow duration-300 cursor-pointer overflow-hidden">
-      <img src="/car01.jpg" className="w-[250px] h-[200px] object-cover" alt="Car" />
+      <img src={car?.imageUrl[0]} className="w-[250px] h-[200px] object-cover" alt="Car" />
       <div className="p-3 flex-1">
-        <h2 className="font-semibold text-4xl text-gray-900">Title</h2>
+        <h2 className="font-semibold text-2xl text-gray-900">{car?.listingTitle}</h2>
         <Separator className="my-2" />
 
         <div className="flex justify-between mt-4 text-gray-600 text-sm">
             <div className="flex flex-col items-center">
                 <LuFuel className="text-xl text-gray-700 mb-1" />
-                <span> Miles</span>
+                <span>{car?.mileage} Miles</span>
             </div>
 
             <div className="flex flex-col items-center">
                 <TbBrandSpeedtest className="text-xl text-gray-700 mb-1" />
-                <span>fuelType</span>
+                <span>{car?.fuelType}</span>
             </div>
 
             <div className="flex flex-col items-center">
                 <GiGearStickPattern className="text-xl text-gray-700 mb-1" />
-                <span>transmission</span>
+                <span>{car?.transmission}</span>
             </div>
 
         </div>
