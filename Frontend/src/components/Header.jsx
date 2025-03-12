9feb86c8f74d3116872b6fa6f39fd6baaff41946
Chamika Jayarathna <failgames.lk@gmail.com>
2025-03-12 23:20:26 +0530

    Update the Header.jsx

--- Frontend/src/components/Header.jsx
+++ Frontend/src/components/Header.jsx
@@ -1,11 +1,13 @@
 import React, { useState, useEffect } from "react";
 import { Button } from "./ui/button";
 import Login from "./Login";
+import UserNavigationPanel from "./UserNavigationPanel";
 
 const Header = () => {
   const [isLoginOpen, setIsLoginOpen] = useState(false);
   const [isAuthenticated, setIsAuthenticated] = useState(false);
   const [isProfileMenuOpen, setIsProfileMenuOpen] = useState(false);
+
 
   useEffect(() => {
     const checkAuth = () => {
@@ -20,12 +22,6 @@ const Header = () => {
       window.removeEventListener("storage", checkAuth);
     };
   }, []);
-  
-
-  const handleLogout = () => {
-    localStorage.removeItem("token");
-    setIsAuthenticated(false);
-  };
 
   return (
     <div className="flex justify-between items-center shadow-sm p-5 relative">
@@ -46,15 +42,10 @@ const Header = () => {
               <img
                 src="https://img.freepik.com/premium-vector/avatar-profile-icon-flat-style-female-user-profile-vector-illustration-isolated-background-women-profile-sign-business-concept_157943-38866.jpg?semt=ais_hybrid"
                 className="w-12 h-12 object-cover rounded-full"
-                alt="Profile"
               />
             </button>
             {isProfileMenuOpen && (
-              <div className="absolute right-0 mt-2 w-40 bg-white shadow-md rounded-lg">
-                <button onClick={handleLogout} className="block px-4 py-2 text-left w-full hover:bg-gray-100">
-                  Logout
-                </button>
-              </div>
+              <UserNavigationPanel/>
             )}
           </div>
         )}
--- Frontend/src/components/UserNavigationPanel.jsx
+++ Frontend/src/components/UserNavigationPanel.jsx
@@ -1,8 +1,18 @@
 import React from 'react';
 
 const UserNavigationPanel = () => {
+   // const handleLogout = () => {
+  //   localStorage.removeItem("token");
+  //   setIsAuthenticated(false);
+  // };
   return (
-    <div>UserNavigationPanel</div>
+    <div className='absolute right-0 mt-2 w-40 bg-white shadow-md rounded-lg'>
+      {/* // <div className="absolute right-0 mt-2 w-40 bg-white shadow-md rounded-lg">
+                    //   <button onClick={handleLogout} className="block px-4 py-2 text-left w-full hover:bg-gray-100">
+                    //     Logout
+                    //   </button>
+                    // </div> */}
+    </div>
   );
 }
 
