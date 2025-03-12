deadc4bcace9beaacebb5fd0ade6603c9e6a46ab
Chamika Jayarathna <failgames.lk@gmail.com>
2025-03-13 00:32:07 +0530

    update the Register.jsx

--- Frontend/src/components/Register.jsx
+++ Frontend/src/components/Register.jsx
@@ -1,48 +1,69 @@
-import React from "react";
+import React, { useState } from "react";
 import { Button } from "./ui/button";
+import { Link } from "react-router-dom";
+import { FaTimesCircle } from "react-icons/fa";
+import { FaEye } from "react-icons/fa";
+import { FaEyeSlash } from "react-icons/fa";
 
 const Register = () => {
+  const [showPassword, setShowPassword] = useState(false);
+
+  const togglePassword = () => {
+    setShowPassword((prevState) => !prevState);
+  };
+
   return (
-    <div className="fixed inset-0 flex items-center justify-center">
-      <div className="bg-white rounded-lg shadow-lg w-96 p-8 h-auto min-h-[450px] flex flex-col justify-center">
-        <h2 className="text-xl font-semibold text-center mb-4">
+    <div className="fixed inset-0 flex items-center justify-center z-10 bg-[rgba(0,0,0,0.5)]">
+      <div className="bg-white rounded-lg shadow-lg w-96 p-8 h-auto min-h-[450px] flex flex-col justify-center relative">
+        <FaTimesCircle className="absolute top-4 right-4 cursor-pointer" />
+        <h2 className="text-xl font-semibold text-center mb-8 mt-4">
           Sign up to Car Marketplace
         </h2>
-        
-        <label className="block text-sm font-medium text-gray-700 mb-1">
-          Full Name
-        </label>
-        <input
-          type="text"
-          placeholder="Enter your full name"
-          className="w-full border border-gray-300 rounded-lg p-3 mb-4 focus:ring focus:ring-indigo-200 focus:border-indigo-500"
-        />
 
-        <label className="block text-sm font-medium text-gray-700 mb-1">
-          Email address
-        </label>
-        <input
-          type="email"
-          placeholder="Enter your email address"
-          className="w-full border border-gray-300 rounded-lg p-3 mb-4 focus:ring focus:ring-indigo-200 focus:border-indigo-500"
-        />
+        <form>
+          <label className="block text-sm font-medium text-gray-700 mb-1">
+            Full Name
+          </label>
+          <input
+            type="text"
+            placeholder="Enter your full name"
+            className="w-full border border-gray-300 rounded-lg p-3 mb-4 focus:ring focus:ring-indigo-200 focus:border-indigo-500"
+          />
 
-        <label className="block text-sm font-medium text-gray-700 mb-1">
-          Password
-        </label>
-        <input
-          type="password"
-          placeholder="Enter your password"
-          className="w-full border border-gray-300 rounded-lg p-3 mb-6 focus:ring focus:ring-indigo-200 focus:border-indigo-500"
-        />
+          <label className="block text-sm font-medium text-gray-700 mb-1">
+            Email address
+          </label>
+          <input
+            type="email"
+            placeholder="Enter your email address"
+            className="w-full border border-gray-300 rounded-lg p-3 mb-4 focus:ring focus:ring-indigo-200 focus:border-indigo-500"
+          />
 
-        <Button className="w-full py-3">Continue</Button>
+          <label className="block text-sm font-medium text-gray-700 mb-1">
+            Password
+          </label>
+          <div className="relative mb-6">
+            <input
+              type={showPassword ? "text" : "password"}
+              placeholder="Enter your password"
+              className="w-full border border-gray-300 rounded-lg p-3 focus:ring focus:ring-indigo-200 focus:border-indigo-500"
+            />
+            <div
+              className="absolute top-1/2 right-3 transform -translate-y-1/2 cursor-pointer"
+              onClick={togglePassword}
+            >
+              {showPassword ? <FaEyeSlash /> : <FaEye />}
+            </div>
+          </div>
 
-        <p className="text-center text-sm text-gray-500 mt-6">
+          <Button className="w-full py-3">Continue</Button>
+        </form>
+
+        <p className="text-center text-sm text-black mt-6">
           Already a member?{" "}
-          <a href="#" className="text-indigo-600 font-medium hover:underline">
+          <Link to={"/login"} className="underline font-medium">
             Sign In
-          </a>
+          </Link>
         </p>
       </div>
     </div>
