ac88e1b4c32c8dd7374b85b4b4a8bb0d2927745f
Chamika Jayarathna <failgames.lk@gmail.com>
2025-03-12 16:25:12 +0530

    update ListingDetails.jsx

--- Frontend/src/listing-details/[id]/ListingDetail.jsx
+++ Frontend/src/listing-details/[id]/ListingDetail.jsx
@@ -1,63 +1,66 @@
-import Header from '@/components/Header';
-import React, { useEffect, useState } from 'react';
-import DetailHeader from '../components/DetailHeader';
-import { useParams } from 'react-router-dom';
-import apiRequest from '@/lib/apiRequest';
-import ImageGallery from '../components/ImageGallery';
-import Description from '../components/Description';
-import Features from '../components/Features';
-import Pricing from '../components/Pricing';
-import Specification from '../components/Specification';
+import Header from "@/components/Header";
+import React, { useEffect, useState } from "react";
+import DetailHeader from "../components/DetailHeader";
+import { useParams } from "react-router-dom";
+import apiRequest from "@/lib/apiRequest";
+import ImageGallery from "../components/ImageGallery";
+import Description from "../components/Description";
+import Features from "../components/Features";
+import Pricing from "../components/Pricing";
+import Specification from "../components/Specification";
+import MostSearchedCar from "@/components/MostSearchedCar";
+import Footer from "@/components/Footer";
 
 const ListingDetail = () => {
+  const { id } = useParams();
+  const [carDetail, setCarDetail] = useState();
 
-    const {id}=useParams();
-    const [carDetail,setCarDetail]=useState();
+  useEffect(() => {
+    GetCarDetail();
+  }, []);
 
-    useEffect(() => {
-        GetCarDetail();
-    },[]);
-
-    const GetCarDetail = async () => {
-        try {
-            const response = await apiRequest.get(`/car-listing/get-single-car-listing/${id}`);
-            if (response.status !== 200) {
-                console.log("Failed to fetch car details.");
-            }
-            const data = response.data;
-            setCarDetail(data);
-        } catch (error) {
-            console.error("Error fetching car details:", error);
-        }
+  const GetCarDetail = async () => {
+    try {
+      const response = await apiRequest.get(
+        `/car-listing/get-single-car-listing/${id}`
+      );
+      if (response.status !== 200) {
+        console.log("Failed to fetch car details.");
+      }
+      const data = response.data;
+      setCarDetail(data);
+    } catch (error) {
+      console.error("Error fetching car details:", error);
     }
+  };
 
   return (
     <div>
-        <Header/>
+      <Header />
 
-        <div className="p-10 md:px-20">
-            {/* Header Detail Component  */}
-            <DetailHeader carDetail={carDetail}/>
+      <div className="p-10 md:px-20">
+        {/* Header Detail Component  */}
+        <DetailHeader carDetail={carDetail} />
 
-            <div className='grid grid-cols-1 md:grid-cols-3 w-full mt-10 gap-5'>
-                {/* Left  */}
-                <div className="md:col-span-2">
-                    <ImageGallery carDetail={carDetail}/>
-                    <Description carDetail={carDetail} />
-                    <Features features={carDetail?.features} />
-                </div>
+        <div className="grid grid-cols-1 md:grid-cols-3 w-full mt-10 gap-5">
+          {/* Left  */}
+          <div className="md:col-span-2">
+            <ImageGallery carDetail={carDetail} />
+            <Description carDetail={carDetail} />
+            <Features features={carDetail?.features} />
+          </div>
 
-                {/* Right  */}
-                <div>
-                    <Pricing carDetail={carDetail} />
-                    <Specification carDetail={carDetail} />
-                </div>
-            </div>
-
+          {/* Right  */}
+          <div>
+            <Pricing carDetail={carDetail} />
+            <Specification carDetail={carDetail} />
+          </div>
         </div>
-
+        <MostSearchedCar />
+      </div>
+      <Footer />
     </div>
   );
-}
+};
 
 export default ListingDetail;
