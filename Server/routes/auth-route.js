3bec25a73c90d481912183391b26abef6001ea5b
Chamika Jayarathna <failgames.lk@gmail.com>
2025-03-12 19:14:26 +0530

    create Login and Signup route

--- Server/routes/auth-route.js
+++ Server/routes/auth-route.js
@@ -1,8 +1,9 @@
 import express from 'express';
-import { Login } from '../controller/auth-controller.js';
+import { Login, Register } from '../controller/auth-controller.js';
 
 const router = express.Router();
 
+router.post('/register', Register);
 router.post('/login', Login);
 
 export default router;
--- Server/schema/User.js
+++ Server/schema/User.js
@@ -1,26 +1,40 @@
 import mongoose from "mongoose";
 
+let profile_imgs_name_list = ["Garfield", "Tinkerbell", "Annie", "Loki", "Cleo", "Angel", "Bob", "Mia", "Coco", "Gracie", "Bear", "Bella", "Abby", "Harley", "Cali", "Leo", "Luna", "Jack", "Felix", "Kiki"];
+let profile_imgs_collections_list = ["notionists-neutral", "adventurer-neutral", "fun-emoji"];
+
 const userSchema = mongoose.Schema({
-    clerkId: {
-        type: String,
-        required: true,
-        unique: true
-    },
-    name: {
+    // clerkId: {
+    //     type: String,
+    //     required: true,
+    //     unique: true
+    // },
+    fullname: {
         type: String,
         required: true
     },
     email: {
         type: String, 
+        required: true,
+        unique: true
+    },
+    password: {
+        type: String,
         required: true 
     },
     role: { 
         type: String, 
         default: "user" 
     },
-    google_auth: {
-        type: Boolean,
-        default: false
+    // google_auth: {
+    //     type: Boolean,
+    //     default: false
+    // },
+    profile_img: {
+        type: String,
+        default: () => {
+            return `https://api.dicebear.com/6.x/${profile_imgs_collections_list[Math.floor(Math.random() * profile_imgs_collections_list.length)]}/svg?seed=${profile_imgs_name_list[Math.floor(Math.random() * profile_imgs_name_list.length)]}`
+        } 
     },
 });
 
