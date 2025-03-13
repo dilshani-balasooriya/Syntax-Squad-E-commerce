bcdefd34565dd27a744a82c2077ab1a62d995d04
Chamika Jayarathna <failgames.lk@gmail.com>
2025-03-13 12:32:31 +0530

    update UserNavigationPanel.jsx

--- Frontend/src/components/UserNavigationPanel.jsx
+++ Frontend/src/components/UserNavigationPanel.jsx
@@ -1,19 +1,24 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const UserNavigationPanel = () => {
-   // const handleLogout = () => {
-  //   localStorage.removeItem("token");
-  //   setIsAuthenticated(false);
-  // };
   return (
-    <div className='absolute right-0 mt-2 w-40 bg-white shadow-md rounded-lg'>
-      {/* // <div className="absolute right-0 mt-2 w-40 bg-white shadow-md rounded-lg">
-                    //   <button onClick={handleLogout} className="block px-4 py-2 text-left w-full hover:bg-gray-100">
-                    //     Logout
-                    //   </button>
-                    // </div> */}
+    <div className="bg-white absolute right-0 border border-gray-300 w-60 shadow-lg rounded-lg p-4">
+      <div className="flex flex-col gap-2">
+
+        <Link className="block px-6 py-3 text-gray-700 rounded-md text-lg hover:bg-gray-100">
+          Profile 
+        </Link>
+
+        <span className="border-t border-gray-300"></span>
+
+        <button className="text-left px-6 py-3 w-full rounded-md hover:bg-gray-100">
+          <h3 className="font-bold text-lg text-gray-800">Sign Out</h3>
+        </button>
+
+      </div>
     </div>
   );
-}
+};
 
 export default UserNavigationPanel;
