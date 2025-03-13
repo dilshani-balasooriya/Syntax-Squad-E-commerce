89f08a3ff28fa2ee07f89712842b97165fe4c275
Chamika Jayarathna <failgames.lk@gmail.com>
2025-03-13 22:14:10 +0530

    update OwnersDetail.jsx

--- Frontend/src/listing-details/components/OwnersDetail.jsx
+++ Frontend/src/listing-details/components/OwnersDetail.jsx
@@ -1,9 +1,35 @@
-import React from 'react';
+import apiRequest from "@/lib/apiRequest";
+import React, { useEffect, useState } from "react";
 
-const OwnersDetail = () => {
+const OwnersDetail = ({ listingId }) => {
+  const [ownerProfileDetail, setOwnerProfileDetail] = useState();
+
+  useEffect(() => {
+    GetOwnersProfile();
+  }, []);
+
+  const GetOwnersProfile = async () => {
+    try {
+      const response = await apiRequest.get(
+        `/auth/get-owner-profile/${listingId}`
+      );
+      const data = response.data;
+      setOwnerProfileDetail(data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   return (
-    <div>OwnersDetail</div>
+    <div className="p-10 border rounded-xl shadow-md mt-7">
+      <h2 className="font-medium text-2xl mb-3">Owner/ Deals</h2>
+      <img
+        src={ownerProfileDetail?.profile_img}
+        className="w-[70px] h-[70px] rounded-full"
+      />
+      <h2 className="mt-2 font-bold text-xl">{ownerProfileDetail?.fullname}</h2>
+    </div>
   );
-}
+};
 
 export default OwnersDetail;
