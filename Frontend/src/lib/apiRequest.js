edb1f475ab6820c9936f046a4533ffc7520ba240
Chamika Jayarathna <failgames.lk@gmail.com>
2025-03-13 16:29:04 +0530

    token bug fixing

--- Frontend/src/lib/apiRequest.js
+++ Frontend/src/lib/apiRequest.js
@@ -4,12 +4,4 @@ const apiRequest = axios.create({
   baseURL: "http://localhost:3000/api",
 });
 
-// apiRequest.interceptors.request.use((config) => {
-//   const token = localStorage.getItem("token");
-//   if (token) {
-//     config.headers.Authorization = `Bearer ${token}`;
-//   }
-//   return config;
-// });
-
 export default apiRequest;
