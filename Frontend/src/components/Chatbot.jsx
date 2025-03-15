cbf53327d53ebd03b417376bd108b8d0301fcd5f
buwanikaveesha <buwanikaveesha@gmail.com>
2025-03-15 20:02:11 +0530

    create chatbot component

--- Frontend/src/components/Chatbot.jsx
+++ Frontend/src/components/Chatbot.jsx
@@ -1,9 +1,29 @@
-import React from 'react';
+import { useEffect } from "react";
 
 const Chatbot = () => {
-    return (
-        <div>Chatbot</div>
-    );
-}
+    useEffect(() => {
+        if (!document.getElementById("chatling-embed-script")) {
+            
+            window.chtlConfig = { chatbotId: import.meta.env.VITE_CHATBOT_KEY };
+
+            const script = document.createElement("script");
+            script.async = true;
+            script.dataset.id = import.meta.env.VITE_CHATBOT_KEY;
+            script.id = "chatling-embed-script";
+            script.type = "text/javascript";
+            script.src = "https://chatling.ai/js/embed.js";
+
+            document.body.appendChild(script);
+
+            return () => {
+                if (script.parentNode) {
+                    script.parentNode.removeChild(script);
+                }
+            };
+        }
+    }, []);
+
+    return null;
+};
 
 export default Chatbot;
