c8133abefd5dddbf86b2df166f5ec037794321b3
Chamika Jayarathna <failgames.lk@gmail.com>
2025-03-16 06:50:32 +0530

    Update ChartBot.jsx

--- Frontend/src/components/Chatbot.jsx
+++ Frontend/src/components/Chatbot.jsx
@@ -1,29 +1,28 @@
 import { useEffect } from "react";
 
 const Chatbot = () => {
-    useEffect(() => {
-        if (!document.getElementById("chatling-embed-script")) {
-            
-            window.chtlConfig = { chatbotId: import.meta.env.VITE_CHATBOT_KEY };
+  useEffect(() => {
+    if (!document.getElementById("chatling-embed-script")) {
+      window.chtlConfig = { chatbotId: import.meta.env.VITE_CHATBOT_KEY };
 
-            const script = document.createElement("script");
-            script.async = true;
-            script.dataset.id = import.meta.env.VITE_CHATBOT_KEY;
-            script.id = "chatling-embed-script";
-            script.type = "text/javascript";
-            script.src = "https://chatling.ai/js/embed.js";
+      const script = document.createElement("script");
+      script.async = true;
+      script.dataset.id = import.meta.env.VITE_CHATBOT_KEY;
+      script.id = "chatling-embed-script";
+      script.type = "text/javascript";
+      script.src = "https://chatling.ai/js/embed.js";
 
-            document.body.appendChild(script);
+      document.body.appendChild(script);
 
-            return () => {
-                if (script.parentNode) {
-                    script.parentNode.removeChild(script);
-                }
-            };
+      return () => {
+        if (script.parentNode) {
+          script.parentNode.removeChild(script);
         }
-    }, []);
+      };
+    }
+  }, []);
 
-    return null;
+  return null;
 };
 
 export default Chatbot;
