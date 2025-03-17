286118ea042df8fe95c8c1fd94a7eb11a63e60b9
Chamika Jayarathna <failgames.lk@gmail.com>
2025-03-17 18:36:35 +0530

    update EditProfile.jsx

--- Frontend/src/components/EditProfile.jsx
+++ Frontend/src/components/EditProfile.jsx
@@ -1,9 +1,91 @@
-import React from 'react';
+import React, { useContext, useState } from "react";
+import { FaEye } from "react-icons/fa";
+import { FaEyeSlash } from "react-icons/fa";
+import { Button } from "./ui/button";
+import AuthContext from "@/context/AuthContext";
+import { jwtDecode } from "jwt-decode";
+import apiRequest from "@/lib/apiRequest";
+import { Toaster } from "react-hot-toast";
 
-const EditProfile = () => {
+const EditProfile = ({ profileDetail }) => {
+  const [showPassword, setShowPassword] = useState(false);
+  const [fullName, setFullName] = useState(profileDetail?.fullname);
+  const [password, setPassword] = useState("");
+
+  const { token } = useContext(AuthContext);
+
+  const togglePassword = () => {
+    setShowPassword((prevState) => !prevState);
+  };
+
+  const handleUpdateProfile = async (e) => {
+    e.preventDefault();
+
+    try {
+      const decodedToken = jwtDecode(token);
+      const userId = decodedToken._id;
+      const updatedData = {
+        fullname: fullName,
+      };
+
+      if (password) {
+        updatedData.password = password;
+      }
+      const response = await apiRequest.put(`/auth/edit-user/${userId}`, updatedData, {
+        headers: { Authorization: `Bearer ${token}` },
+      });
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   return (
-    <div>EditProfile</div>
+    <>
+      <Toaster />
+      <div className="max-w-3xl mx-auto p-8 mt-10">
+        <div className="flex flex-col items-center gap-3 w-full text-center">
+          <form onSubmit={handleUpdateProfile}>
+            <img
+              src={profileDetail?.profile_img}
+              className="w-32 h-32 object-cover rounded-full border-4 border-gray-300 shadow-md transition-transform duration-300 hover:scale-105"
+            />
+            <label className="block text-sm font-medium text-gray-700 mb-1">
+              Full Name
+            </label>
+            <input
+              type="text"
+              placeholder="Enter your full name"
+              className="w-full border border-gray-300 rounded-lg p-3 mb-4 focus:ring focus:ring-indigo-200 focus:border-indigo-500"
+              value={fullName}
+              onChange={(e) => setFullName(e.target.value)}
+            />
+            <label className="block text-sm font-medium text-gray-700 mb-1">
+              Password
+            </label>
+            <div className="relative mb-6">
+              <input
+                type={showPassword ? "text" : "password"}
+                placeholder="Enter your password"
+                className="w-full border border-gray-300 rounded-lg p-3 focus:ring focus:ring-indigo-200 focus:border-indigo-500"
+                onChange={(e) => setPassword(e.target.value)}
+              />
+              <div
+                className="absolute top-1/2 right-3 transform -translate-y-1/2 cursor-pointer"
+                onClick={togglePassword}
+              >
+                {showPassword ? <FaEyeSlash /> : <FaEye />}
+              </div>
+            </div>
+
+            <div className="flex gap-4">
+              <Button type="submit">Update Profile</Button>
+              <Button type="button">Back to Profile</Button>
+            </div>
+          </form>
+        </div>
+      </div>
+    </>
   );
-}
+};
 
 export default EditProfile;
--- Frontend/src/components/ProfileCom.jsx
+++ Frontend/src/components/ProfileCom.jsx
@@ -4,11 +4,12 @@ import { jwtDecode } from "jwt-decode";
 import apiRequest from "@/lib/apiRequest";
 import { Button } from "./ui/button";
 import { useNavigate } from "react-router-dom";
+import EditProfile from "./EditProfile";
 
 const ProfileCom = () => {
   const [profileDetail, setProfileDetail] = useState();
+  const [isEditing, setIsEditing] = useState(false);
   const { token, removeFromSession } = useContext(AuthContext);
-
   const navigate = useNavigate();
 
   useEffect(() => {
@@ -36,30 +37,28 @@ const ProfileCom = () => {
   return (
     <div className="max-w-3xl mx-auto bg-gradient-to-r from-gray-50 to-gray-100 p-8 rounded-lg shadow-lg mt-10">
       <div className="flex flex-col items-center gap-3 w-full text-center">
-        <img
-          src={profileDetail?.profile_img}
-          alt="profile_img"
-          className="w-32 h-32 object-cover rounded-full border-4 border-gray-300 shadow-md transition-transform duration-300 hover:scale-105"
-        />
+        {!isEditing ? (
+          <>
+            <img
+              src={profileDetail?.profile_img}
+              alt="profile_img"
+              className="w-32 h-32 object-cover rounded-full border-4 border-gray-300 shadow-md transition-transform duration-300 hover:scale-105"
+            />
+            <h1 className="text-3xl font-bold text-gray-800">{profileDetail?.fullname}</h1>
+            <p className="text-gray-600 text-base">{profileDetail?.email}</p>
 
-        <h1 className="text-3xl font-bold text-gray-800">
-          {profileDetail?.fullname}
-        </h1>
-
-        <p className="text-gray-600 text-base">
-          {profileDetail?.email || "user@example.com"}
-        </p>
-
-        <div className="flex gap-4 mt-4 w-full max-w-xs">
-          <Button className="w-full">Edit Profile</Button>
-          <Button
-            onClick={handleLogout}
-            variant="destructive"
-            className="w-full"
-          >
-            Logout
-          </Button>
-        </div>
+            <div className="flex gap-4 mt-4 w-full max-w-xs">
+              <Button className="w-full" onClick={() => setIsEditing(true)}>
+                Edit Profile
+              </Button>
+              <Button onClick={handleLogout} variant="destructive" className="w-full">
+                Logout
+              </Button>
+            </div>
+          </>
+        ) : (
+          <EditProfile profileDetail={profileDetail} />
+        )}
       </div>
     </div>
   );
