4ffbcf84756646f2a1f5a172cf39ba3f8d0f3068
Chamika Jayarathna <failgames.lk@gmail.com>
2025-03-17 16:59:05 +0530

    update PDF geneter part FuelTypeChart v1

--- Frontend/src/admin/dashboard/components/FuelTypeChart.jsx
+++ Frontend/src/admin/dashboard/components/FuelTypeChart.jsx
@@ -66,9 +66,9 @@ const FuelTypeChart = ({ fuelType }) => {
         <button
           ref={buttonRef}
           onClick={exportToPDF}
-          className="px-4 py-2 text-white rounded-lg"
+          className="px-3 py-3 text-white rounded-full hover:bg-gray-700 transition-colors duration-200"
         >
-          <File />
+          <File size={20}/>
         </button>
       </div>
 
