01486f7d3d65991a4f90ecc86e6f257a56ca9981
Chamika Jayarathna <failgames.lk@gmail.com>
2025-03-26 00:55:39 +0530

    update imageGallery.jsx

--- Frontend/src/listing-details/components/ImageGallery.jsx
+++ Frontend/src/listing-details/components/ImageGallery.jsx
@@ -1,12 +1,31 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 const ImageGallery = ({ carDetail }) => {
+  const [selectedImage, setSelectedImage] = useState(0);
+
+  useEffect(() => {
+    if (carDetail?.imageUrl && carDetail.imageUrl.length > 0) {
+      setSelectedImage(0);
+    }
+  }, [carDetail?.imageUrl]);
+
   return (
     <div>
       <img
-        src={carDetail?.imageUrl[0]}
+        src={carDetail?.imageUrl[selectedImage]}
         className="w-full h-[500px] object-cover rounded-xl"
       />
+      <div className="flex gap-2 mb-6 overflow-x-auto py-2">
+        {carDetail?.imageUrl?.map((img, index) => (
+          <div
+            key={index}
+            onClick={() => setSelectedImage(index)}
+            className="cursor-pointer flex-shrink-0 w-20 h-16 rounded-md overflow-hidden border border-gray-400"
+          >
+            <img src={img} className="w-full h-full object-cover" />
+          </div>
+        ))}
+      </div>
     </div>
   );
 };
